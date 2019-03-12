b8a8c72fb7aa6d2ba00360a6c737ee8ad45bb4f8
A <benjy@benjy.dk>
2019-03-12 08:26:55 +0100

    Eclipse Project

--- CheckersTest/src/CheckersTest.java
+++ CheckersTest/src/CheckersTest.java
@@ -6,9 +6,26 @@ class CheckersTest {
 	Board b = new Board();
 	WhitePlayer white = new WhitePlayer();
 	BlackPlayer black = new BlackPlayer();
+	Turn t = new Turn();
+	
 	@Test
-	void test() {
-		System.out.println("hello");
+	void testTurnSwitch() {
+		int i = 0;
+		while(i<3) {
+		switch(t.getSilentTurn()) {
+		case "1":
+			assertEquals(t.getSilentTurn(),"1");
+			t.changeTurn();
+			i++;
+		break;
+		case "2":
+			assertEquals(t.getSilentTurn(), "2");
+			t.changeTurn();
+			i++;
+		break;
+		
+		}
+		}
 	}
 
 }
